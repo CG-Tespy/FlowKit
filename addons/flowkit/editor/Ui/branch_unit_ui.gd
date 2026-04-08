6d2baaa0ff5e4e3b6779058cc869f67dd16e941c
CG-Tespy <gbcabreracgt@gmail.com>
2026-04-08 10:50:04 -0400

    Branch add_action ui now responds to both left and right click

--- addons/flowkit/editor/Ui/branch_unit_ui.gd
+++ addons/flowkit/editor/Ui/branch_unit_ui.gd
@@ -144,7 +144,7 @@ func _toggle_negate() -> void:
 
 func _on_add_action_input(event: InputEvent) -> void:
 	var we_want_to_respond: bool = event is InputEventMouseButton and event.pressed and \
-	event.button_index == MOUSE_BUTTON_RIGHT
+	(event.button_index == MOUSE_BUTTON_RIGHT or event.button_index == MOUSE_BUTTON_LEFT)
 	if we_want_to_respond:
 		_flash_label(add_action_label)
 		_show_add_action_context_menu()
