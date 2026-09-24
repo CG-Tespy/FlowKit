784861a5d331e255bc082560ace4681708dd3df9
CG-Tespy <gbcabreracgt@gmail.com>
2026-09-24 09:04:44 -0400

    Refinements to var ui formatting

--- addons/flowkit/editor/Ui/Variables/fk_bool_variable_ui.gd
+++ addons/flowkit/editor/Ui/Variables/fk_bool_variable_ui.gd
@@ -4,6 +4,13 @@ class_name FKBoolVariableUi
 
 @export var value_field: CheckBox
 
+func _enter_tree() -> void:
+	super._enter_tree()
+	_update_value_field_text()
+
+func _update_value_field_text():
+	value_field.text = "On" if value_field.button_pressed else "Off"
+
 func _toggle_subs(wants_subs_active: bool) -> void:
 	if wants_subs_active and not _is_subbed:
 		value_field.toggled.connect(_commit_value)
@@ -13,3 +20,5 @@ func _toggle_subs(wants_subs_active: bool) -> void:
 
 func _set_value(value: Variant) -> void:
 	value_field.button_pressed = bool(value)
+	_update_value_field_text()
+	
